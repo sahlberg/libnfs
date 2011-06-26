a9b241622fa5b87da3140e03a0db7c15f39a48d4
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2011-06-26 18:22:45 +1000

    add a sockaddr_storage to the rpc context so we can store the socket a upd packet came in from

--- include/libnfs-private.h
+++ include/libnfs-private.h
@@ -41,6 +41,7 @@ struct rpc_context {
 
        /* special fields for UDP, which can sometimes be BROADCASTed */
        int is_udp;
+       struct sockaddr *udp_dest;
        int is_broadcast;
 };
 
