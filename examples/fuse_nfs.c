8ca7b275f81a8bea97290e90edd8aa2e127dc697
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2013-11-03 21:01:11 -0800

    We don't need -odebug for the fuse nfs client anymore

--- examples/fuse_nfs.c
+++ examples/fuse_nfs.c
@@ -214,14 +214,14 @@ int main(int argc, char *argv[])
 	char *url = NULL;
 	char *mnt = NULL;
 	char *server = NULL, *export = NULL, *strp;
-	int fuse_nfs_argc = 6;
+	int fuse_nfs_argc = 5;
 	char *fuse_nfs_argv[16] = {
 		"fuse-nfs",
 		"<export>",
-		"-odebug",
 		"-oallow_other",
 		"-odefault_permissions",
 		"-omax_write=32768",
+		NULL,
 		NULL,
 		NULL,
 		NULL,
