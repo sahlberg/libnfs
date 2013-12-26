b70567b62c3adc90cd5f52ad869963bb4bd14435
Peter Lieven <pl@kamp.de>
2013-12-26 12:48:21 +0100

    nfs_parse_url: initialize memory
    the urls struct was accidently left unitialized.
    
    Signed-off-by: Peter Lieven <pl@kamp.de>

--- lib/libnfs.c
+++ lib/libnfs.c
@@ -201,6 +201,7 @@ static struct nfs_url *nfs_parse_url(struct nfs_context *nfs, const char *url, i
 		return NULL;
 	}
 
+	memset(urls, 0x00, sizeof(struct nfs_url));
 	urls->server = strdup(url + 6);
 	if (urls->server == NULL) {
 		nfs_destroy_url(urls);
