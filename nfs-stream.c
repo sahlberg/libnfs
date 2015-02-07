6f6832b6c19a26557883af924d7135f5ef87f45d
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2015-02-07 12:43:59 -0800

    nfs-stream.c: a simple test tool to check performance sequential read mode
    ./nfs-stream <mfs-url> <bytes-per-second-to-read>
    
    This tool is for testing sequential read mode.
    It reads 32kbyte one block at a time using the sync API.
    The idea is that when a nfs_[p]read() command is executed, we will once we
    identify a sequential scan start issuing asynchronous commands in the
    background to populate a readahead cache.
    
    During read, we try to service the request immediately out of cache
    and return immediately to the application. During read from cache we will also
    process any to previous reads and use it to populate the cache. Also, as the
    file offset grows, we will discard the already read blocks from the cache
    and issue new asynchronous commands at the tail of the cache.
    
    nfs_read() will when the cache is operating behave like :
      nfs_read() {
         ... process any previous reads and populate the cache ...
         ... send async command to replenish the tail of the cache ...
         return data from cache straigh back to application
      }
    
    The test tool will try to read 32kb chunks at a given rate.
    Once the test tool manages to service from cache  the read laency should drop
    to <<1ms and the majority of the time spent will be in the sleep() between calls.
    Once the latency is steady at <<1ms this is an indication that we now
    service all reads out of cache instead of the network.
    If the read latency remains <<1ms continously, without any spikes, it means that
    we have been successfull in maintaining the asycntrhous tasks to replenish the cache.
    
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

---
+++ nfs-stream.c
@@ -0,0 +1,65 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "nfsc/libnfs.h"
+
+int main(int argc, char *argv[])
+{
+	struct nfs_context *nfs;
+	struct nfs_url *url;
+	struct nfsfh *nfsfh = NULL;
+	struct nfs_stat_64 st;
+	uint64_t offset;
+	char buf[32768];
+	struct timeval t1, t2;
+	uint64_t delta, tpc;
+
+	nfs = nfs_init_context();
+	url = nfs_parse_url_full(nfs, argv[1]);
+	if (!url) {
+		fprintf(stderr, "Can not parse URL. %s\n",
+			nfs_get_error(nfs));
+		exit(1);
+	}
+	if (nfs_mount(nfs, url->server, url->path) != 0) {
+		fprintf(stderr, "Failed to mount nfs share : %s\n",
+			       nfs_get_error(nfs));
+		exit(1);
+	}
+	if (nfs_open(nfs, url->file, O_RDONLY, &nfsfh) != 0) {
+		fprintf(stderr, "Failed to open file %s: %s\n",
+			url->file,
+			nfs_get_error(nfs));
+		exit(1);
+	}
+	if (nfs_fstat64(nfs, nfsfh, &st)) {
+		fprintf(stderr, "Failed to stat file %s: %s\n",
+			url->file,
+			nfs_get_error(nfs));
+		exit(1);
+	}
+	printf("File size:%lld\n", (long long)st.nfs_size);
+	tpc = 1000000 / (strtol(argv[2], NULL, 10) / 32768);
+	printf("Read one 32kb chunk every %d us\n", (int)tpc);
+	for (offset = 0; offset < st.nfs_size; offset += 32768) {
+		gettimeofday(&t1, NULL);
+		nfs_read(nfs, nfsfh, 8192, buf);
+		gettimeofday(&t2, NULL);
+		delta = t2.tv_sec * 1000000LL + t2.tv_usec -
+		  t1.tv_sec * 1000000LL - t1.tv_usec;
+		printf("Read latency:%lld us\n", (long long)delta);
+		if (tpc > delta) {
+			printf("Sleep for %d us\n", (int)(tpc - delta));
+			usleep(tpc - delta);
+		}
+	}
+
+	nfs_close(nfs, nfsfh);
+	nfs_destroy_context(nfs);	
+	nfs_destroy_url(url);
+	return 0;
+}
