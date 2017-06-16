bbf38735d84e5e9f1a3205c6893802b450252b98
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2017-06-16 12:37:42 -0700

    TESTS: Initial test directory.
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

---
+++ tests/functions.sh
@@ -0,0 +1,25 @@
+export TGT_IPC_SOCKET=`pwd`/tgtd.socket 
+
+TESTDIR=`pwd`/testdata
+TESTSHARE="127.0.0.1:${TESTDIR}"
+TESTURL="nfs://127.0.0.1/${TESTDIR}"
+
+start_share() {
+    mkdir "${TESTDIR}" 2>/dev/null
+    exportfs -o rw,no_root_squash "${TESTSHARE}"
+}
+
+stop_share() {
+    exportfs -u "${TESTSHARE}"
+    rm -rf "${TESTDIR}"
+}
+
+success() {
+    echo "[OK]"
+}
+
+failure() {
+    echo "[FAILED]"
+    exit 1
+}
+
