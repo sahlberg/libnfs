8fa13b3a90ce7997fc1fada0a193cd279efa8402
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2017-07-01 09:21:57 +1000

    TESTS: improve softlink tests and validate the links are visible locally
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- tests/test_0700_symlink_basic.sh
+++ tests/test_0700_symlink_basic.sh
@@ -10,9 +10,17 @@ echo -n "Create a symlink from a root path (absolute) ... "
 ./prog_symlink "${TESTURL}/" kangabanga /symlink || failure
 success
 
+echo -n "Verify the link ... "
+ls -l "${TESTDIR}/symlink" | egrep "\-> kangabanga$" >/dev/null || failure
+success
+
 echo -n "Create a symlink from a nested path (absolute) ... "
 mkdir "${TESTDIR}/subdir"
 ./prog_symlink "${TESTURL}/" kangabanga /subdir/symlink || failure
+success
+
+echo -n "Verify the link ... "
+ls -l "${TESTDIR}/subdir/symlink" | egrep "\-> kangabanga$" >/dev/null || failure
 success
 
 stop_share
