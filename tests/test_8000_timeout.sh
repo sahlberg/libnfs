f1c7b96610bcdb5467ee272e7a4af05fd5f1cb17
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2017-08-15 20:15:43 +1000

    NFSv4: We only need 18 events to stat a file in v4. Update the timeout test.
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- tests/test_8000_timeout.sh
+++ tests/test_8000_timeout.sh
@@ -4,10 +4,16 @@
 
 echo "NFSv${VERS} Basic timeout test."
 
+if [ $VERS -eq 3 ]; then
+    COUNT=28
+else
+    COUNT=18
+fi
+
 start_share
 
 touch "${TESTDIR}/testfile"
-for IDX in `seq 1 28`; do
+for IDX in `seq 1 $COUNT`; do
     echo -n "Test timeout at socket event ${IDX} ... "
     TIMEOUT_START=${IDX} LD_PRELOAD=./ld_timeout.so ./prog_stat "${TESTURL}/?version=${VERS}" "." testfile >/dev/null 2>&1 && failure
     success
