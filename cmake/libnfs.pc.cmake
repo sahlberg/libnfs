7969c7829126262a743006a21a35e7382126e3bd
Lukas Rusak <lorusak@gmail.com>
2017-09-29 14:07:22 -0700

    add cmake build system support

---
+++ cmake/libnfs.pc.cmake
@@ -0,0 +1,14 @@
+# libnfs pkg-config file
+
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@INSTALL_LIB_DIR@
+includedir=@INSTALL_INC_DIR@
+
+Name: libnfs
+Description: libnfs is a client library for accessing NFS shares over a network.
+Version: @PROJECT_VERSION@
+Requires:
+Conflicts:
+Libs: -L${libdir} -lnfs
+Cflags: -I${includedir}
