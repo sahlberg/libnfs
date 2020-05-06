b9c85afc4f3d010da69a58afb010353875f6fc64
DetlefGolze <detlef.golze@point.de>
2020-05-06 10:51:39 +0200

    Update libnfs-zdr.c
    libnfs_zdr_array() needs to determine element count before calculating size to allocate.

--- lib/libnfs-zdr.c
+++ lib/libnfs-zdr.c
@@ -319,14 +319,14 @@ bool_t libnfs_zdr_array(ZDR *zdrs, char **arrp, uint32_t *size, uint32_t maxsize
 	int  i;
         uint32_t s;
 
+	if (!libnfs_zdr_u_int(zdrs, size)) {
+		return FALSE;
+	}
+
         if (*size > UINT32_MAX/elsize) {
                 return FALSE;
         }
         s = *size * elsize;
-
-	if (!libnfs_zdr_u_int(zdrs, size)) {
-		return FALSE;
-	}
 
 	if (zdrs->x_op == ZDR_DECODE) {
 		*arrp = zdr_malloc(zdrs, s);
