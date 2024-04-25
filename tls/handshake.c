56942a04e30b8c304d5939117653f693a7e17c39
Nagendra Tomar <natomar@microsoft.com>
2024-04-25 13:35:23 +0000

    Added copyright.

--- tls/handshake.c
+++ tls/handshake.c
@@ -1,4 +1,20 @@
 /* -*-  mode:c; tab-width:8; c-basic-offset:8; indent-tabs-mode:nil;  -*- */
+/*
+   Copyright (C) 2024 by Linuxsmiths <linuxsmiths@gmail.com>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; either version 2.1 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
