5a5932612652cfad0a1edd23a837d4487cad0fd0
Nagendra Tomar <natomar@microsoft.com>
2024-06-21 13:13:41 +0000

    Fix indent

--- nfs/nfs.c
+++ nfs/nfs.c
@@ -282,9 +282,9 @@ rpc_nfs3_read_task(struct rpc_context *rpc, rpc_cb cb,
 		return NULL;
 	}
 
-    pdu->in.buf = buf;
-    pdu->in.len = count;
-    pdu->requested_read_count = count;
+        pdu->in.buf = buf;
+        pdu->in.len = count;
+        pdu->requested_read_count = count;
 
 	if (rpc_queue_pdu(rpc, pdu) != 0) {
 		rpc_set_error(rpc, "Out of memory. Failed to queue pdu for NFS3/READ call");
